9bc2a3c84920f079b56fa6720bda55f39e1f8002
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-07-24 20:02:12 +0000

    task 0

--- main.h
+++ main.h
@@ -6,5 +6,15 @@
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
-int handle_conversion_specifier(const char *format, va_list args);
+int binary_printf(const char *format, ...);
+int custom_printf(const char *format, ...);
+int flag_printf(const char *format, ...);
+int length_printf(const char *format, ...);
+int field_width_printf(const char *format, ...);
+int precision_printf(const char *format, ...);
+int zero_flag_printf(const char *format, ...);
+int minus_flag_printf(const char *format, ...);
+int custom_reversed_printf(const char *format, ...);
+int custom_rot13_printf(const char *format, ...);
+int all_options_printf(const char *format, ...);
 #endif /* MAIN_H */
--- task0.c
+++ task0.c
@@ -1,16 +1,9 @@
 #include "main.h"
 
-/**
-* _printf - produces output according to a format.
-* @format: character string containing directives.
-*
-* Return: the number of characters printed (excluding the null byte)
-*/
 int _printf(const char *format, ...)
 {
 va_list args;
-int count = 0;
-
+int char_count = 0;
 va_start(args, format);
 
 while (*format)
@@ -18,56 +11,37 @@ while (*format)
 if (*format == '%')
 {
 format++;
-count += handle_conversion_specifier(format, args);
+if (*format == 'c')
+{
+int c = va_arg(args, int);
+putchar(c);
+char_count++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+while (*str)
+{
+putchar(*str);
+str++;
+char_count++;
+}
+}
+else if (*format == '%')
+{
+putchar('%');
+char_count++;
+}
 }
 else
 {
-write(1, format, 1);
-count++;
+putchar(*format);
+char_count++;
 }
-
 format++;
 }
 
 va_end(args);
-
-return (count);
+return (char_count);
 }
 
-/**
-* handle_conversion_specifier - handles the conversion specifiers.
-* @format: character string containing directives.
-* @args: variable argument list.
-*
-* Return: the number of characters printed for the conversion specifier.
-*/
-int handle_conversion_specifier(const char *format, va_list args)
-{
-char c;
-char *s;
-
-switch (*format)
-{
-case 'c':
-c = va_arg(args, int);
-write(1, &c, 1);
-return (1);
-case 's':
-s = va_arg(args, char *);
-int count = 0;
-while (*s)
-{
-write(1, s, 1);
-s++;
-count++;
-}
-return (count);
-case '%':
-write(1, "%", 1);
-return (1);
-default:
-write(1, "%", 1);
-write(1, format, 1);
-return (2);
-}
-}
