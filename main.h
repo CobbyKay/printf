db3b1f18665798a9e472469f632b5d064353016b
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-07-24 19:28:57 +0000

    task0.c

--- main.h
+++ main.h
@@ -3,7 +3,8 @@
 
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
-
+int handle_conversion_specifier(const char *format, va_list args);
 #endif /* MAIN_H */
--- task0.c
+++ task0.c
@@ -10,8 +10,6 @@ int _printf(const char *format, ...)
 {
 va_list args;
 int count = 0;
-char c;
-char *s;
 
 va_start(args, format);
 
@@ -20,32 +18,7 @@ while (*format)
 if (*format == '%')
 {
 format++;
-switch (*format)
-{
-case 'c':
-c = va_arg(args, int);
-write(1, &c, 1);
-count++;
-break;
-case 's':
-s = va_arg(args, char *);
-while (*s)
-{
-write(1, s, 1);
-s++;
-count++;
-}
-break;
-case '%':
-write(1, "%", 1);
-count++;
-break;
-default:
-write(1, "%", 1);
-write(1, format, 1);
-count += 2;
-break;
-}
+count += handle_conversion_specifier(format, args);
 }
 else
 {
@@ -59,4 +32,42 @@ format++;
 va_end(args);
 
 return (count);
+}
+
+/**
+* handle_conversion_specifier - handles the conversion specifiers.
+* @format: character string containing directives.
+* @args: variable argument list.
+*
+* Return: the number of characters printed for the conversion specifier.
+*/
+int handle_conversion_specifier(const char *format, va_list args)
+{
+char c;
+char *s;
+
+switch (*format)
+{
+case 'c':
+c = va_arg(args, int);
+write(1, &c, 1);
+return (1);
+case 's':
+s = va_arg(args, char *);
+int count = 0;
+while (*s)
+{
+write(1, s, 1);
+s++;
+count++;
+}
+return (count);
+case '%':
+write(1, "%", 1);
+return (1);
+default:
+write(1, "%", 1);
+write(1, format, 1);
+return (2);
+}
 }
