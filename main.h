f3908b6516d2e9076417dc5723adc567710e911e
Richardkomla <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@github.com>
2023-07-25 08:08:21 -0700

    another

--- main.h
+++ main.h
@@ -1,10 +1,25 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <unistd.h>
 #include <stdarg.h>
+/**
+ * struct print - Structure containing identifiers
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
 int print_i(va_list i);
 int print_d(va_list d);
-#endif /* MAIN_H */
+int print_b(va_list b);
+int print_R(va_list R);
+
+#endif
