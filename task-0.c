0bb31e0a8e52b2752d3b3e47dd3d77450864080e
Kwame Acheampong-Cobbiah <acobby99@gmail.com>
2023-07-25 18:15:37 +0000

    0 and 1

--- task-0.c
+++ task-0.c
@@ -1,5 +1,7 @@
 #include "main.h"
-
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
 /**
 * _printf - produces output according to a format.
 * @format: character string containing directives.
